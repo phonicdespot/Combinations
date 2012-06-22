f818883e153c11b316bf9fd8d943fa79e8390e7a
samgrainger <samuel.grainger@gmail.com>
2012-06-22 23:57:45 +0100

    Generalised to n rows, basic UI, commented

--- Combinations/Form1.Designer.vb
+++ Combinations/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Combinations
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,29 +22,122 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Combinations))
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Cols = New System.Windows.Forms.NumericUpDown()
+        Me.Min = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Max = New System.Windows.Forms.NumericUpDown()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.Cols, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Min, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Max, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Location = New System.Drawing.Point(249, 21)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(331, 114)
+        Me.Button1.Size = New System.Drawing.Size(59, 25)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Execute"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Form1
+        'Cols
+        '
+        Me.Cols.Location = New System.Drawing.Point(167, 25)
+        Me.Cols.Name = "Cols"
+        Me.Cols.Size = New System.Drawing.Size(76, 20)
+        Me.Cols.TabIndex = 1
+        Me.Cols.Value = New Decimal(New Integer() {6, 0, 0, 0})
+        '
+        'Min
+        '
+        Me.Min.Location = New System.Drawing.Point(15, 25)
+        Me.Min.Maximum = New Decimal(New Integer() {30, 0, 0, 0})
+        Me.Min.Name = "Min"
+        Me.Min.Size = New System.Drawing.Size(59, 20)
+        Me.Min.TabIndex = 3
+        Me.Min.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(164, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Columns:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(80, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(16, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "to"
+        '
+        'Max
+        '
+        Me.Max.Location = New System.Drawing.Point(102, 25)
+        Me.Max.Maximum = New Decimal(New Integer() {31, 0, 0, 0})
+        Me.Max.Name = "Max"
+        Me.Max.Size = New System.Drawing.Size(59, 20)
+        Me.Max.TabIndex = 6
+        Me.Max.Value = New Decimal(New Integer() {4, 0, 0, 0})
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Range:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(60, 68)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(214, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "See Immediate Window (Ctrl + G) for output."
+        '
+        'Combinations
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(356, 141)
+        Me.ClientSize = New System.Drawing.Size(322, 90)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Max)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Min)
+        Me.Controls.Add(Me.Cols)
         Me.Controls.Add(Me.Button1)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Combinations"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Combinations"
+        CType(Me.Cols, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Min, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Max, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Cols As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Min As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Private WithEvents Max As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label4 As System.Windows.Forms.Label
 
 End Class
--- Combinations/Form1.vb
+++ Combinations/Form1.vb
@@ -1,40 +1,59 @@
-﻿Public Class Form1
+﻿
+
+Public Class Combinations
+    'Given wider scope for the benefit of ap(),
+    'the array printing subroutine.
+    Dim intCols As Integer
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Dim intNums() As Integer = New Integer() {0, 0, 0, 0, 0, 0}
+        'Sets the number of columns. Zero indexed :P
+        intCols = Me.Cols.Value - 1
 
-        ap(intNums)
+        Dim intNums(intCols) As Integer
 
-        For x = 1 To 4
-            intNums(0) = 0
-            intNums(1) = 0
-            intNums(2) = 0
-            intNums(3) = 0
-            intNums(4) = 0
-            intNums(5) = x
+        For x = Me.Min.Value To Me.Max.Value
+            'Resets all but the rightmost array value to 0
+            For o = 0 To (intCols - 1)
+                intNums(o) = 0
+            Next
+            'Increments the rightmost value by 1
+            intNums(intCols) = x
 
+            'Do loop iterates until leftmost value equals rightmost value
+            'i.e. all rows involving x have been exhausted.
             Do Until intNums(0) = x
-                For i = 0 To 5
-                    Dim r As Integer
-                    Dim l As Integer
+                'Outputs array before modification so that first row is
+                'present (first row is initial input)
+                ap(intNums)
 
-                    If i = 5 Then
-                        r = 5
+                '  For loop "Scans" across a row of numbers, comparing the
+                'current value, intNums(i), with those either side:
+                'intNums(left) and intNums(right).
+                '  Increasing any number breaks out of the loop to ensure
+                'that all unique "rows" are output.
+                For i = 0 To intCols
+                    Dim right As Integer
+                    Dim left As Integer
+
+                    'If statements ensure that left and right counters
+                    'do not become negative at the far ends of each row.
+                    If i = intCols Then
+                        right = intCols
                     Else
-                        r = i + 1
+                        right = i + 1
                     End If
 
                     If i = 0 Then
-                        l = 0
+                        left = 0
                     Else
-                        l = i - 1
+                        left = i - 1
                     End If
-                    'Debug.Print(i & " input:")
-                    'ap(intNums)
-                    'Debug.Print("i:" & intNums(i) & " l:" & intNums(l) & " r:" & intNums(r))
-                    If intNums(i) < intNums(r) And intNums(l) = intNums(i) Then
+
+                    'Increases current number by one if: right hand number is greater
+                    If intNums(i) < intNums(right) And intNums(left) = intNums(i) Then
                         intNums(i) = intNums(i) + 1
-                        'Debug.Print("boom")
+                        'If the current number is increased to a value above 1, all
+                        'numbers to the left are reset to 0
                         If intNums(i) > 1 Then
                             For m = 0 To (i - 1)
                                 intNums(m) = 0
@@ -42,26 +61,23 @@
                         End If
                         Exit For
                     End If
-                    'Debug.Print(i & " result:")
-                    'ap(intNums)
                 Next
-
             Loop
+            'Outputs array after final iteration of for loop to make
+            'up for the result being output before processing (see above)
+            ap(intNums)
         Next
     End Sub
 
-
+    'ap(<Array>) prints an array to the immediate window.
     Private Sub ap(ByVal intNums As Integer())
         Dim strPrint As String
         Dim strOut As String
-
         strOut = ""
-
-        For i = 0 To 5
-            strPrint = intNums(i)
+        For i = 0 To intCols
+            strPrint = intNums(i) & " "
             strOut = strOut & strPrint
         Next
-
         Debug.Print(strOut)
     End Sub
 
